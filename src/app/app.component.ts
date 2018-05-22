80648a1f43d9c15a4e5ef5b4d47ec0e28856a794
Vivek <vivek@viveks-air.home>
2018-05-21 21:24:00 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { variable, DeclareVarStmt } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My Todos';
+  todos = [
+  {
+    label: 'milk',
+    done: false,
+    no: 2
+  },
+  {
+    label: 'Buttermilk',
+    done: true,
+    no: 3
+  },
+  {
+    label: 'Sugarmilk',
+    done: false,
+    no: 5
+  },
+  {
+    label: 'cleanmilk',
+    done: true,
+    no: 6
+  },
+];
+
+
+ addTodo(newTodolabel) {
+  // tslint:disable-next-line:prefer-const
+  // tslint:disable-next-line:no-var-keyword
+  // tslint:disable-next-line:prefer-const
+   const newTodo = {
+    label: newTodolabel,
+    no: 1,
+    done: false
+  };
+  this.todos.push(newTodo);
+}
+
+deleteTodo(todo) {
+  this.todos = this.todos.filter(t => t.label !== todo.label);
+}
+markAsDoneTodo(todo) {
+  todo.done = true;
+}
+
 }
